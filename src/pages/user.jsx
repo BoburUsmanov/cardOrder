3f724c0dbf4de9efb2664a8d7bbfd62447122624
BoburUsmanov <boburusmanov1994@gmail.com>
2019-08-23 10:35:44 +0300

    userpage

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,33 +1,50 @@
-import React, { Component } from 'react';
-import {user_entered} from './../redux/actions/actions';
-import {connect} from 'react-redux';
-import {reactLocalStorage} from 'reactjs-localstorage';
-import {Link} from 'react-router-dom';
+import React, { Component } from "react";
+import { user_entered } from "./../redux/actions/actions";
+import { connect } from "react-redux";
+import { reactLocalStorage } from "reactjs-localstorage";
+import { Link } from "react-router-dom";
 import { Redirect } from "react-router";
 
 class User extends Component {
-    constructor(props){
-        super(props)
-        let enter = localStorage.setItem('rememberMe', true);
-      }
-    componentDidMount(){
-        localStorage.setItem('rememberMe', true);
-    }
-    Exit = (e) =>{
-        localStorage.clear();
-    }
-    render() {
-      
-        return (
-            <div style={{display:'flex',justifyContent:'space-between'}}>
-               <p> User page</p>
-                <Link to="/" className="reload" onClick = {this.Exit}>EXIT</Link>
-            </div>
-        )
-    }
-}
-const mapStateToProps = state =>({
-    entered:state.session.session.status
-})
+  constructor(props) {
+    super(props);
+    let rememberMe = localStorage.getItem("rememberMe");
+    this.state = {
+      rememberMe: rememberMe
+    };
+  }
 
-export default connect(mapStateToProps,{user_entered})(User)
+  componentDidMount() {
+    if (this.props.entered === 0) {
+      localStorage.setItem("rememberMe", true);
+    }
+    // alert(localStorage.getItem('rememberMe'))
+  }
+
+  render() {
+    const logout = e => {
+      localStorage.clear();
+    };
+
+    if (!localStorage.getItem('rememberMe')) {
+      return <Redirect to="/" />;
+    }
+
+    return (
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <p> User page</p>
+        <Link to="/" className="reload" onClick={logout}>
+          Logout
+        </Link>
+      </div>
+    );
+  }
+}
+const mapStateToProps = state => ({
+  entered: state.session.session.status
+});
+
+export default connect(
+  mapStateToProps,
+  { user_entered }
+)(User);
